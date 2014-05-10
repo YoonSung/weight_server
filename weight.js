8640d7a82528d4d817d980b3ca20d01139820d41
Yoon <lvev9925@naver.com>
2014-05-10 11:31:16 +0900

    Refactoring requestQuery function, Add getList Process

--- weight.js
+++ weight.js
@@ -27,89 +27,70 @@ var app = express();
 app.use(express.bodyParser());//({uploadDir:__dirname + '/images'}));
 
 
-
-
-
-app.get('/', function(request, response)
-{
-	fs.readFile('HTMLPage.html', function(error, data)
-	{
-		response.send(data.toString());
-	});
-});
-
 app.post('/upload', function(request, response) {
-//	console.log(request.body);
-//	console.log(request.files);
-	
-	console.log(request.body.id);
-	console.log(request.body.isMan);
-	console.log(request.body.weight);
-	console.log(request.body.language);
-	console.log(request.body.path);
 	
 	fs.readFile(request.files.image.path, function(error, data) {
 			var filePath = __dirname + "/images/" + request.files.image.name;
 			console.log(filePath);
 			
-			fs.writeFile(filePath, data, function(err) {
-				if (error) {
-					response.json({ result: "fail" });
-					//response.send(err);
-				} else {
-					
-					requestQuery(
-						//error
-						//"INSERT tbl_weight(id, isman, weight, language, path) values( ??, ??, ??, ??, ??);"
+		fs.writeFile(filePath, data, function(err) {
+			if (error) {
+				response.send("fail");
+				//response.send(err);
+			} else {
+				
+				requestQuery(			
+					"INSERT INTO tbl_weight(id, isMan, weight, language, path) values( ?, ?, ?, ?, ? ) "
+					,[request.body.id, request.body.isMan, request.body.weight, request.body.language, request.body.path]
+					,function(err, oResult) {
+						if (error) {
+							response.send("fail");
+						}
 						
-						
-						"INSERT INTO tbl_weight(id, isMan, weight, language, path) values("
-						+"'" + request.body.id + "',"
-					 	+ request.body.isMan + ","
-						+ request.body.weight + ","
-						+"'" + request.body.language + "',"
-						+"'" + request.body.path + "');"
-						
-						, [request.body.id, request.body.isMan, request.body.weight, request.body.language, request.body.path]
-					);
-					
-					//response.send(filePath);
-					response.json({ result: "success" });
-				}	
-			});
+						if (oResult["affectedRows"] === 1) {
+							response.send("true");	
+						} else {
+							response.send("false");
+						}
+					}
+				);
+			}	
 		});
-	
+	});
 	//response.redirect('/');
 });
 
-app.get('/upload', function(request, response) {
+app.post('/getList', function(request, response) {
 	
-//	requestQuery(
-//		"SELECT * FROM tbl_weight WHERE language = (SELECT language FROM tbl_weight WHERE id ='"+request.body.id+"');";
-//	);
+	console.log("getList / body : "+request.body);
 	
-	console.log(request.body);
+	requestQuery(			
+		"SELECT * FROM tbl_weight WHERE language = (SELECT language FROM tbl_weight WHERE id = ?)"
+		,[request.body.id]
+		,function(err, aResult) {
+			if (error) {
+				response.send("fail");
+			}
+			
+			response.send(aResult);
+		}
+	);
 });
 
-
 //Execute Query
-function requestQuery(sql, ainsertValues) {
+function requestQuery(sql, aInsertValues, callbackFunction) {
 	
 	//sql QueryGenerator. 
 	//Like Java PreparedStatement Class
-//	var sql = mysql.format(sql, ainsertValues);
-	
-	
-	
-	//===test code
-	console.log(sql);
-	
+	var sql = mysql.format(sql, aInsertValues);	
 	
 	var resultData = pool.getConnection(function(err, connection) {
-		
+		//test
+		console.log("sql : ", sql);
 		//when error occur
+		
 		if (err) {
-			console.log("error occur");
+			console.log("error occur : ",err);
 		}
 		
 		//connection request
@@ -120,82 +101,14 @@ function requestQuery(sql, ainsertValues) {
 				console.log("Generate Sql Query Failed!!");
 			}
 			
-			console.log("Query Success!!!!!!");
-			//===test code
-			console.log("results : ", queryResult);
-			
-			return queryResult;
+			callbackFunction(err, queryResult);
 		});
 	});
-	
-	console.log("query : ", resultData);
-//	console.log("query.sql : ", query.sql);
-}
-
-
+};
 
 //server
-http.createServer(app).listen(9000, function() {
+http.createServer(app).listen(8080, function() {
 	console.log('Server running');
 });
 
 console.log('\u001b[0m');
-
-
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// 
-// var app = express();
-// app.use(bodyParser());
-// 
-// var fs = require('fs');
-// var http = require('http');
-// 
-// //bodyParser 설정
-// //Client Page의 Tag에 접근하기 위해서 필요
-// //app.use(bodyParser({uploadDir: __dirname + '/images'}));
-// //app.use(express.limit('10mb'));
-// //app.use(app.router);
-// express.multipart();
-// 
-// app.get('/', function(request, response) {
-// 	console.log("in /");
-// 	
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.write("Hello\r\n");
-//     response.write("World\r\n");
-//     response.end();
-// 	console.log("hello hello");
-// });
-// 
-// 
-// //html test
-// app.get('/main', function (request, response) {
-// 	fs.readFile('main.html', function (error, data) {
-// 		response.writeHead(200, {'Content-Type': 'text/html'});
-// 		response.end(data);
-// 	});
-// })
-// 
-// 
-// app.post('/upload', function(request, response) {
-// 	//bodyParser가 없을 경우  request.files 에서 에러발생
-// 
-// 	console.log(request.body);
-// 	console.log(request.body.test);
-// 	
-// 	// fs.readFile(request.files.uploadFile.path, function(error, data) {
-// 	// 	var filePath = __dirname + "\\files\\" + request.files.uploadFile.name;
-// 	// 	
-// 	// 	if (error) {
-// 	// 		throw err;
-// 	// 	} else {
-// 	// 		response.redirect()
-// 	// 	}
-// 	// });
-// });
-// 
-// 
-// http.createServer(app).listen(9000, function(error, data) {
-// 	console.log('Server running');
-// });
