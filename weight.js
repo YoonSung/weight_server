85cc72d2ad531f12b098a223c9b94878763ac497
Yoon <lvev9925@naver.com>
2014-05-11 09:30:30 +0900

    Add Casting Process

--- weight.js
+++ weight.js
@@ -27,52 +27,62 @@ var app = express();
 app.use(express.bodyParser());//({uploadDir:__dirname + '/images'}));
 
 
+// app.get('/', function(request, response)
+// {
+// 	fs.readFile('HTMLPage.html', function(error, data)
+// 	{
+// 		response.send(data.toString());
+// 	});
+// });
+
 app.post('/upload', function(request, response) {
 	
 	fs.readFile(request.files.image.path, function(error, data) {
 			var filePath = __dirname + "/images/" + request.files.image.name;
 			console.log(filePath);
 			
-		fs.writeFile(filePath, data, function(err) {
-			if (error) {
-				response.send("fail");
-				//response.send(err);
-			} else {
-				
-				requestQuery(			
-					"INSERT INTO tbl_weight(id, isMan, weight, language, path) values( ?, ?, ?, ?, ? ) "
-					,[request.body.id, request.body.isMan, request.body.weight, request.body.language, request.body.path]
-					,function(oResult) {
+			fs.writeFile(filePath, data, function(err) {
+				if (error) {
+					response.send("fail");
+					//response.send(err);
+				} else {
+					
+					requestQuery(			
+						"INSERT INTO tbl_weight(id, isMan, weight, language, path) values( ?, ?, ?, ?, ? ) "
+//						+"'" + request.body.id + "',"
+//					 	+ request.body.isMan + ","
+//						+ request.body.weight + ","
+//						+"'" + request.body.language + "',"
+//						+"'" + request.body.path + "');"
 						
-						if (oResult["affectedRows"] === 1) {
-							response.send("true");	
-						} else {
-							response.send("false");
+						, [request.body.id, new Boolean(request.body.isMan), parseFloat(request.body.weight), request.body.language, request.body.path]
+						function(err, oResult) {
+							if (error) {
+								response.send("fail");
+							} else {
+								if (oResult["affectedRows"] === 1)
+									response.send("true");
+									else
+							}
 						}
-					}
-				);
-			}	
+					);
+					
+					//response.send(filePath);
+					//response.json("true");
+					response.send("true");
+				}	
+			});
 		});
-	});
+
 	//response.redirect('/');
 });
 
 app.post('/getList', function(request, response) {
-	
-	console.log("getList / body : "+request.body);
-	
-	requestQuery(			
-		"SELECT * FROM tbl_weight WHERE language = (SELECT language FROM tbl_weight WHERE id = ?)"
-		,[request.body.id]
-		,function(err, aResult) {
-			if (error) {
-				response.send("fail");
-				return;
-			}
-			
-			response.send(aResult);
-		}
-	);
+//	requestQuery(
+//		"SELECT * FROM tbl_weight WHERE language = (SELECT language FROM tbl_weight WHERE id ='"+request.body.id+"');";
+//	);
+	console.log("test!!!!!!!!!!!!!!!!!!!!!!");
+	console.log(request.body);
 });
 
 //Execute Query
@@ -89,7 +99,6 @@ function requestQuery(sql, aInsertValues, callbackFunction) {
 		
 		if (err) {
 			console.log("error occur : ",err);
-			return;
 		}
 		
 		//connection request
@@ -111,3 +120,62 @@ http.createServer(app).listen(8080, function() {
 });
 
 console.log('\u001b[0m');
+
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// 
+// var app = express();
+// app.use(bodyParser());
+// 
+// var fs = require('fs');
+// var http = require('http');
+// 
+// //bodyParser 설정
+// //Client Page의 Tag에 접근하기 위해서 필요
+// //app.use(bodyParser({uploadDir: __dirname + '/images'}));
+// //app.use(express.limit('10mb'));
+// //app.use(app.router);
+// express.multipart();
+// 
+// app.get('/', function(request, response) {
+// 	console.log("in /");
+// 	
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello\r\n");
+//     response.write("World\r\n");
+//     response.end();
+// 	console.log("hello hello");
+// });
+// 
+// 
+// //html test
+// app.get('/main', function (request, response) {
+// 	fs.readFile('main.html', function (error, data) {
+// 		response.writeHead(200, {'Content-Type': 'text/html'});
+// 		response.end(data);
+// 	});
+// })
+// 
+// 
+// app.post('/upload', function(request, response) {
+// 	//bodyParser가 없을 경우  request.files 에서 에러발생
+// 
+// 	console.log(request.body);
+// 	console.log(request.body.test);
+// 	
+// 	// fs.readFile(request.files.uploadFile.path, function(error, data) {
+// 	// 	var filePath = __dirname + "\\files\\" + request.files.uploadFile.name;
+// 	// 	
+// 	// 	if (error) {
+// 	// 		throw err;
+// 	// 	} else {
+// 	// 		response.redirect()
+// 	// 	}
+// 	// });
+// });
+// 
+// 
+// http.createServer(app).listen(9000, function(error, data) {
+// 	console.log('Server running');
+// });
