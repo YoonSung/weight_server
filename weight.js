656c48b7fd0e09d380188274730d24cb07b63f41
Yoon <lvev9925@naver.com>
2014-05-16 13:58:16 +0900

    Delete Obsolete Files And Comments

--- weight.js
+++ weight.js
@@ -36,8 +36,7 @@ app.use(express.bodyParser());//({uploadDir:__dirname + '/images'}));
 // 	});
 // });
 app.get('/image', function(request, response) {
-//	console.log(request.body);
-//	console.log(request.query.id);
+	
 	var filePath = __dirname + "/images/" + request.query.id+".png";
 	console.log("filePath : "+filePath);
 	
@@ -142,62 +141,3 @@ http.createServer(app).listen(8080, function() {
 });
 
 console.log('\u001b[0m');
-
-
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// 
-// var app = express();
-// app.use(bodyParser());
-// 
-// var fs = require('fs');
-// var http = require('http');
-// 
-// //bodyParser 설정
-// //Client Page의 Tag에 접근하기 위해서 필요
-// //app.use(bodyParser({uploadDir: __dirname + '/images'}));
-// //app.use(express.limit('10mb'));
-// //app.use(app.router);
-// express.multipart();
-// 
-// app.get('/', function(request, response) {
-// 	console.log("in /");
-// 	
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.write("Hello\r\n");
-//     response.write("World\r\n");
-//     response.end();
-// 	console.log("hello hello");
-// });
-// 
-// 
-// //html test
-// app.get('/main', function (request, response) {
-// 	fs.readFile('main.html', function (error, data) {
-// 		response.writeHead(200, {'Content-Type': 'text/html'});
-// 		response.end(data);
-// 	});
-// })
-// 
-// 
-// app.post('/upload', function(request, response) {
-// 	//bodyParser가 없을 경우  request.files 에서 에러발생
-// 
-// 	console.log(request.body);
-// 	console.log(request.body.test);
-// 	
-// 	// fs.readFile(request.files.uploadFile.path, function(error, data) {
-// 	// 	var filePath = __dirname + "\\files\\" + request.files.uploadFile.name;
-// 	// 	
-// 	// 	if (error) {
-// 	// 		throw err;
-// 	// 	} else {
-// 	// 		response.redirect()
-// 	// 	}
-// 	// });
-// });
-// 
-// 
-// http.createServer(app).listen(9000, function(error, data) {
-// 	console.log('Server running');
-// });
